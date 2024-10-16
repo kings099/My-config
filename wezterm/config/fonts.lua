09c1bfc5d3a94ee737f1a94ef612190afa1b10dc
Hengyu King <hengyuking@gmail.com>
2024-10-17 01:26:37 +0800

    reupload wezterm

---
+++ wezterm/config/fonts.lua
@@ -0,0 +1,23 @@
+--[[
+Author: mikami520 yl.xiao@mail.utoronto.ca
+Date: 2024-03-28 09:58:26
+LastEditors: mikami520 yl.xiao@mail.utoronto.ca
+LastEditTime: 2024-03-28 11:25:40
+FilePath: /wezterm/config/fonts.lua
+Description: 
+I Love IU
+Copyright (c) 2024 by mikami520 yl.xiao@mail.utoronto.ca, All Rights Reserved. 
+--]]
+local wezterm = require('wezterm')
+local platform = require('utils.platform')
+local font = "MesloLGSDZ Nerd Font Mono"
+local font_size = platform().is_mac and 20 or 9
+
+return {
+   font = wezterm.font(font, {italic=false}),
+   font_size = font_size,
+
+   --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
+   freetype_load_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
+   freetype_render_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
+}
